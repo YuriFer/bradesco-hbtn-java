b24f07363f6fc889f5b32a28a9b9223cdfced1d7
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-07-31 14:31:08 +0000

    Atividade de estrutura de dados ArrayList

---
+++ estruturas_de_dados/1/GestaoAluno.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class GestaoAluno {
+    private ArrayList<Aluno> listaAlunos;
+
+    public GestaoAluno() {
+        this.listaAlunos = new ArrayList<>();
+    }
+
+    public void adicionarAluno(String nome, int idade) {
+        Aluno novoAluno = new Aluno(nome, idade);
+        listaAlunos.add(novoAluno);
+    }
+
+    public void removerAluno(String nome) {
+        for (Aluno aluno : listaAlunos) {
+            if (aluno.getNome().equals(nome)) {
+                listaAlunos.remove(aluno);
+            }
+        }
+    }
+
+    public String buscarAluno(String nome) {
+        for (Aluno aluno : listaAlunos) {
+            if (aluno.getNome().equals(nome)) {
+                return aluno.toString();
+            }
+        }
+        return null;
+    }
+
+    public void listarAlunos() {
+        for (Aluno aluno : listaAlunos) {
+            System.out.println(aluno);
+        }
+    }
+
+    public static void main(String[] args) {
+        GestaoAluno gestao = new GestaoAluno();
+
+        gestao.adicionarAluno("Joao", 20);
+        gestao.adicionarAluno("Maria", 22);
+        gestao.adicionarAluno("Pedro", 19);
+
+        gestao.listarAlunos();
+
+        String alunoBuscado = gestao.buscarAluno("Joao");
+        if (alunoBuscado != null) {
+            System.out.println("Aluno encontrado: " + alunoBuscado);
+        } else {
+            System.out.println("Aluno nao encontrado.");
+        }
+
+        gestao.removerAluno("Maria");
+        gestao.listarAlunos();
+    }
+}
