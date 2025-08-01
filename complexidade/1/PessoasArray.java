165e2a98da5ea488de9bd45ed24cdf8ce643435b
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-08-01 19:14:10 +0000

    Ajustes log

--- complexidade/1/PessoasArray.java
+++ complexidade/1/PessoasArray.java
@@ -20,22 +20,16 @@ public class PessoasArray {
     // implementar o método de buscaLinear
     public void buscaLinear(String nome){
         boolean encontrado = false;
-
         System.out.println(String.format("Procurando pelo nome: \"%s\"", nome));
-
-
         for (int i = 0; i < nomes.length; i++) {
             System.out.println("Passando pelo indice:" + i);
             if (nomes[i] == nome) {
                 encontrado = true;
                 System.out.println(String.format("Nome pesquisado é %s que está na posição %d", nomes[i], i));
             }
-
-
         }
-
         if(!encontrado){
-                throw new IllegalArgumentException(String.format("O nome %s não se encontra no array de nomes", nome));
+            throw new IllegalArgumentException(String.format("O nome %s não se encontra no array de nomes", nome));
         }
     }
 }
