11425eccf4272e43552204310af1c12c09dea734
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-08-06 15:11:40 +0000

    Atividade 0. Armazém de itens

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Armazem<Item> implements Armazenavel<Item> {
+    private Map<String, Item> itens;
+
+    public Armazem(){
+        this.itens = new HashMap<String,Item>();
+    }
+
+    @Override
+    public void adicionarAoInventario(String nome, Item valor) {
+        itens.put(nome, valor);
+    }
+
+    @Override
+    public Item obterDoInventario(String nome) {
+        return itens.get(nome);
+    }
+    
+}
