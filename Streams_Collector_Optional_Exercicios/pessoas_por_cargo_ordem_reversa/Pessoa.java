d8b9ebca1905678d4c85a5e7d9f340e45d36603a
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-08-17 19:07:24 +0000

    Atividade stream collector 0. Pessoas agrupadas por cargos em order reversa

---
+++ Streams_Collector_Optional_Exercicios/pessoas_por_cargo_ordem_reversa/Pessoa.java
@@ -0,0 +1,45 @@
+public class Pessoa implements Comparable<Pessoa>{
+    private int codigo;
+    private String nome;
+    private String cargo;
+    private int idade;
+    private double salario;
+
+    public Pessoa(int codigo, String nome, String cargo, int idade, double salario) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargo = cargo;
+        this.idade = idade;
+        this.salario = salario;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%d] %s %s %d R$ ", codigo, nome, cargo, idade) + String.format(java.util.Locale.FRANCE, "%f", salario);
+    }
+
+    @Override
+    public int compareTo(Pessoa pessoa) {
+        return Double.compare(this.salario, pessoa.salario);
+    }
+}
