bea7953331acfff7ed61b4e282dc6326cfaff86d
Yuri Fernandes <yuri_fernandes2000@hotmail.com>
2025-09-14 14:25:30 -0300

    Ajustes atividade thread

--- threads/0/Principal.java
+++ threads/0/Principal.java
@@ -1,15 +1,18 @@
 public class Principal {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         Contador contador = new Contador();
-        ThreadContador[] threads = new ThreadContador[2];
 
-        for (int i = 0; i < threads.length; i++) {
-            threads[i] = new ThreadContador(contador);
-            threads[i].start();
-        }
+        ThreadContador t1 = new ThreadContador(contador);
+        ThreadContador t2 = new ThreadContador(contador);
 
-        for (ThreadContador thread : threads) {
-            thread.join();
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
         System.out.println("Valor final do contador: " + contador.getContagem());
